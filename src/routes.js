c7c5e16786bc55fec094d3883c79652dfbd32d41
Matheus Santana <matheus.santana@ccc.ufcg.edu.br>
2020-04-17 13:01:50 -0300

    feature: some JWT validation

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,45 @@
-const express = require("express");
-const { User } = require("./models");
+const express = require('express');
+const { User } = require('./models');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const routes = express.Router();
 
-routes.post("/users", async (req, res) => {
+routes.post('/users/signUp', async (req, res) => {
   const { email, name, password } = req.body;
+  const hashPassword = await bcrypt.hash(password, 10);
 
-  const newUser = await User.create({ name, email, password });
+  const newUser = await User.create({ name, email, password: hashPassword });
 
   return res.status(200).json(newUser);
 });
 
-routes.get("/status", (req, res) => {
-  return res.status(200).json({ status: "OK" });
+routes.post('/users/signIn', async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findByPk(email);
+
+  console.log(user);
+
+  if (!user) return res.status(400).json({ message: 'User not found!' });
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordValid) return res.status(400).send('Invalid password!');
+
+  const token = jwt.sign(
+    {
+      name: user.name,
+      email: user.email,
+    },
+    'secret',
+    { expiresIn: 60 * 60 }
+  );
+
+  return res.status(200).json(token);
+});
+
+routes.get('/status', (req, res) => {
+  return res.status(200).json({ status: 'OK' });
 });
 
 module.exports = routes;
