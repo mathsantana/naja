0bc282f77a38cec25b8b6681e518759c6fb8b95b
mathsantana <matheus.santana@ccc.ufcg.edu.br>
2020-04-25 16:47:45 -0300

    feature: auth DONE

--- src/routes.js
+++ src/routes.js
@@ -1,45 +1,30 @@
-const express = require('express');
-const { User } = require('./models');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const {
+  signIn,
+  signUp,
+  googleSignIn,
+  googleCallBack,
+  authMiddleware,
+} = require("./controllers/User");
 
 const routes = express.Router();
 
-routes.post('/users/signUp', async (req, res) => {
-  const { email, name, password } = req.body;
-  const hashPassword = await bcrypt.hash(password, 10);
+routes.post("/user/signUp", signUp);
 
-  const newUser = await User.create({ name, email, password: hashPassword });
+routes.post("/user/signIn", signIn);
 
-  return res.status(200).json(newUser);
+routes.get("/auth/google", googleSignIn);
+
+routes.get("/auth/google/callback", googleCallBack);
+
+routes.get("/status", (req, res) => {
+  return res.status(200).json({ status: "OK" });
 });
 
-routes.post('/users/signIn', async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findByPk(email);
-
-  console.log(user);
-
-  if (!user) return res.status(400).json({ message: 'User not found!' });
-
-  const isPasswordValid = await bcrypt.compare(password, user.password);
-
-  if (!isPasswordValid) return res.status(400).send('Invalid password!');
-
-  const token = jwt.sign(
-    {
-      name: user.name,
-      email: user.email,
-    },
-    'secret',
-    { expiresIn: 60 * 60 }
-  );
-
-  return res.status(200).json(token);
-});
-
-routes.get('/status', (req, res) => {
-  return res.status(200).json({ status: 'OK' });
+routes.get("/private", authMiddleware, (req, res, next) => {
+  return res.status(200).json({ status: "Authenticated" });
 });
 
 module.exports = routes;
