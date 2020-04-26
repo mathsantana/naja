c7ad5af0bdaeff9590a63ca483c236dc32fd4450
Matheus Santana <matheus.santana@ccc.ufcg.edu.br>
2020-04-26 17:33:38 -0300

    hotfix: heroku port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const routes = require("./routes");
+const express = require('express');
+const routes = require('./routes');
 
-const PORT = 3333;
+const PORT = process.env.PORT || 3333;
 const app = express();
 
 // Necessario para o Express receber o formato JSON
@@ -9,6 +9,6 @@ app.use(express.json());
 // Carregar o arquivo rotas
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log("API online na porta", PORT);
+app.listen(PORT, () => {
+  console.log('API online na porta', PORT);
 });
