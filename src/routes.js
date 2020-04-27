d27f537b625cc92f9954e49c97c77e0f6f8a19d3
Matheus Santana <matheus.santana@ccc.ufcg.edu.br>
2020-04-27 02:32:29 -0300

    hotfix: CORS

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 
 const {
   signIn,
@@ -13,6 +14,16 @@ const Category = require('./controllers/Category');
 const Product = require('./controllers/Product');
 
 const routes = express.Router();
+
+routes.use((req, res, next) => {
+  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+  res.header('Access-Control-Allow-Origin', '*');
+  //Quais são os métodos que a conexão pode realizar na API
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  console.log('Aconteceu!');
+  routes.use(cors());
+  next();
+});
 
 routes.post('/user/signUp', signUp);
 routes.post('/user/signIn', signIn);
